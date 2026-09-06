456d738931e9435bb69a02f5959ba21f2c18afb0
Maicol Fontecha <mdfontechad@gmail.com>
2026-09-06 12:12:18 -0500

    Creating type and some objects, initializing with npmstart

---
+++ daily/2026-09-04_TYPESCRIPT FirstTask/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = {
+    id: 1,
+    tittle: "primera",
+    completed: false,
+    priority: "low",
+    tags: ["xd"]
+};
+console.log(a.id);
+console.log('a.id :>> ', a.id);
+//# sourceMappingURL=index.js.map
--- daily/2026-09-04_TYPESCRIPT FirstTask/src/index.ts
+++ daily/2026-09-04_TYPESCRIPT FirstTask/src/index.ts
@@ -6,6 +6,45 @@ type Task= {
  tags:string[]
 }
 
-function createTask():void{
-    
-} 
+let TaskA: Task={
+    id:1,
+    tittle:"primera",
+    completed:false,
+    priority:"low",
+    tags:["xd"]
+}
+let TaskB: Task={
+    id:1,
+    tittle:"segunda",
+    completed:false,
+    priority:"high",
+    tags:["xd"]
+}
+let TaskC: Task={
+    id:1,
+    tittle:"tercera",
+    completed:true,
+    priority:"low",
+    tags:["xd"]
+}
+let TaskD: Task={
+    id:1,
+    tittle:"cuarta",
+    completed:false,
+    priority:"high",
+    tags:["xd"]
+}
+let TaskE: Task={
+    id:1,
+    tittle:"ultima",
+    completed:true,
+    priority:"low",
+    tags:["xd"]
+}
+
+let everyTask: Task[]=[TaskA,TaskB,TaskC,TaskD,TaskE];
+
+const getCompletedTasks=(AllTasks:Task[]):Task[]=>{
+
+    return [TaskE]
+}
