4b6ceda8c0a4e8ab3af7321ae8ffaa63b6a325eb
Maicol Fontecha <mdfontechad@gmail.com>
2026-09-08 07:23:53 -0500

    Starting and understanding first API

---
+++ daily/2026-09-08_TYPESCRIPT fistAPI/src/server.ts
@@ -0,0 +1,23 @@
+
+const fastify = require('fastify')({
+  logger: true
+})
+declare const process: {
+  exit(code?: number): never;
+};
+
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+    fastify.log.info(`server listening on ${address}`)
+  // Server is now listening on ${address}
+})
